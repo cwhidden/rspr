6f61167111f721eed51db35133a5e1e755d2a3cb
Christopher Whidden <whidden@titan.research.cs.dal.ca>
2011-10-11 11:09:31 -0300

    Revert "fixed bug in sync_twins"
    This reverts commit 35f18a244f15ad63a9f367bca323335c6ce251fb.

--- Forest.h
+++ Forest.h
@@ -446,29 +446,17 @@ bool sync_twins(Forest *T1, Forest *T2) {
 		Node *T2_a = T2_labels[i];
 		if (T1_a == NULL && T2_a != NULL) {
 			Node *node = T2_a->parent();
-			if (node == NULL)
+			if (node == NULL
+					|| ( node->lchild()->is_leaf() && node->rchild()->is_leaf()))
 				return false;
-			if (node->parent() == NULL && node->lchild()->is_leaf() && node->rchild()->is_leaf()) {
-				return false;
-				Node *sibling = node->lchild();
-				if (sibling == T2_a)
-						sibling = node->rchild();
-				T2_labels[stomini(sibling->str())] = sibling;
-			}
 			delete T2_a;
 			node->contract(true);
 		}
 		else if (T2_a == NULL && T1_a != NULL) {
 			Node *node = T1_a->parent();
-			if (node == NULL)
+			if (node == NULL
+					|| ( node->lchild()->is_leaf() && node->rchild()->is_leaf()))
 				return false;
-			if (node->parent() == NULL && node->lchild()->is_leaf() && node->rchild()->is_leaf()) {
-				return false;
-				Node *sibling = node->lchild();
-				if (sibling == T1_a)
-						sibling = node->rchild();
-				T1_labels[stomini(sibling->str())] = sibling;
-			}
 			delete T1_a;
 			node->contract(true);
 			
@@ -483,15 +471,9 @@ bool sync_twins(Forest *T1, Forest *T2) {
 		Node *T1_a = T1_labels[i];
 		if (T1_a != NULL) {
 			Node *node = T1_a->parent();
-			if (node == NULL)
+			if (node == NULL
+					|| ( node->lchild()->is_leaf() && node->rchild()->is_leaf()))
 				return false;
-			if (node->parent() == NULL && node->lchild()->is_leaf() && node->rchild()->is_leaf()) {
-				return false;
-				Node *sibling = node->lchild();
-				if (sibling == T1_a)
-						sibling = node->rchild();
-				T1_labels[stomini(sibling->str())] = sibling;
-			}
 			delete T1_a;
 			node->contract(true);
 			
@@ -501,15 +483,9 @@ bool sync_twins(Forest *T1, Forest *T2) {
 		Node *T2_a = T2_labels[i];
 		if (T2_a != NULL) {
 			Node *node = T2_a->parent();
-			if (node == NULL)
+			if (node == NULL
+					|| ( node->lchild()->is_leaf() && node->rchild()->is_leaf()))
 				return false;
-			if (node->parent() == NULL && node->lchild()->is_leaf() && node->rchild()->is_leaf()) {
-				return false;
-				Node *sibling = node->lchild();
-				if (sibling == T2_a)
-						sibling = node->rchild();
-				T2_labels[stomini(sibling->str())] = sibling;
-			}
 			delete T2_a;
 			node->contract(true);
 		}
